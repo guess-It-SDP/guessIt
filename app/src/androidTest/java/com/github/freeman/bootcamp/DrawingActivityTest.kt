b3c486ff66c78791a21314ddaa1df11662f57302
Pyr0ss <pguillon67@gmail.com>
2023-03-15 16:04:53 +0100

    added test

---
+++ app/src/androidTest/java/com/github/freeman/bootcamp/DrawingActivityTest.kt
@@ -0,0 +1,20 @@
+package com.github.freeman.bootcamp
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DrawingActivityTest {
+    @get:Rule
+    val composeRule = createAndroidComposeRule<DrawingActivity>()
+
+    @Test
+    fun drawingScreenIsDisplayed() {
+        composeRule.onNode(hasTestTag("drawingScreen")).assertIsDisplayed()
+    }
+}
--- app/src/main/java/com/github/freeman/bootcamp/DrawingActivity.kt
+++ app/src/main/java/com/github/freeman/bootcamp/DrawingActivity.kt
@@ -7,12 +7,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.freeman.bootcamp.ui.theme.BootcampComposeTheme
 import io.ak1.drawbox.DrawBox
@@ -23,13 +22,7 @@ class DrawingActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BootcampComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    DrawingScreen{}
-                }
+                DrawingScreen{}
             }
         }
     }
@@ -37,11 +30,13 @@ class DrawingActivity : ComponentActivity() {
 
 @Composable
 fun DrawingScreen(save: (Bitmap) -> Unit) {
-    Box {
+    Box (Modifier.testTag("drawingScreen")){
         Column {
             val controller = rememberDrawController()
             DrawBox(
                 drawController = controller,
+                modifier = Modifier
+                    .fillMaxSize(),
                 bitmapCallback = { imageBitmap, error ->
                     imageBitmap?.let {
                         save(it.asAndroidBitmap())
@@ -55,8 +50,6 @@ fun DrawingScreen(save: (Bitmap) -> Unit) {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    BootcampComposeTheme {
-        DrawingScreen{}
-    }
+fun DrawingScreenPreview() {
+    DrawingScreen{}
 }
