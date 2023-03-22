1378c1da5e0bd383cf3a9218652f4d5bbf5641c2
Naarileth <michael.freeman@epfl.ch>
2023-03-22 19:38:43 +0100

    For an obscure reason tests for the guessing activity were commented

--- app/src/androidTest/java/com/github/freeman/bootcamp/GuessingTest.kt
+++ app/src/androidTest/java/com/github/freeman/bootcamp/GuessingTest.kt
@@ -2,7 +2,6 @@ package com.github.freeman.bootcamp
 
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.test.espresso.Espresso
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.freeman.bootcamp.ui.theme.BootcampComposeTheme
 import com.google.firebase.database.DatabaseReference
@@ -12,78 +11,78 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-//@RunWith(AndroidJUnit4::class)
-//class GuessingTest {
-//    @get:Rule
-//    val composeRule = createComposeRule()
-//
-//    @Test
-//    fun guessingScreenIsDisplayed() {
-//        initScreenWithDatabase()
-//        composeRule.onNodeWithTag("guessingScreen").assertIsDisplayed()
-//    }
-//
-//    @Test
-//    fun guessingScreenContainsCorrectText() {
-//        initScreenWithDatabase()
-//        composeRule.onNodeWithTag("guessText").assertTextContains("Your turn to guess!")
-//    }
-//
-//    @Test
-//    fun guessesListIsDisplayed() {
-//        initScreenWithDatabase()
-//        composeRule.onNodeWithTag("guessesList").assertIsDisplayed()
-//    }
-//
-//    @Test
-//    fun guessingBarIsDisplayed() {
-//        initScreenWithDatabase()
-//        composeRule.onNodeWithTag("guessingBar").assertIsDisplayed()
-//    }
-//
-//    @Test
-//    fun guessingPreviewDisplaysGuessingScreen() {
-//        initScreenWithoutDatabase()
-//        composeRule.onNodeWithTag("guessingScreen").assertIsDisplayed()
-//        composeRule.onNodeWithTag("guessText").assertTextContains("Your turn to guess!")
-//        composeRule.onNodeWithTag("guessesList").assertIsDisplayed()
-//        composeRule.onNodeWithTag("guessingBar").assertIsDisplayed()
-//    }
-//
-//    @Test
-//    fun guessIsDisplayedInGuessingList() {
-//        initScreenWithDatabase()
-//        composeRule.onNode(hasSetTextAction()).performTextInput("House")
-//        composeRule.onNodeWithTag("guessButton").performClick()
-//        composeRule.onNodeWithTag("guessItem").onChild().assertIsDisplayed()
-//    }
-//
-//
-//    private fun initGuessingDataBase(): DatabaseReference {
-//        val db = Firebase.database
-//        val guessGameId = "GameTestGuessesId"
-//        db.useEmulator("10.0.2.2", 9000)
-//        return Firebase.database.getReference("Guesses/$guessGameId")
-//    }
-//
-//    private fun initScreenWithoutDatabase() {
-//        composeRule.setContent {
-//            BootcampComposeTheme {
-//                GuessingPreview()
-//            }
-//        }
-//    }
-//
-//    private fun initScreenWithDatabase() {
-//        val database = initGuessingDataBase()
-//
-//        composeRule.setContent {
-//            BootcampComposeTheme {
-//                GuessingScreen(database)
-//            }
-//        }
-//    }
-//
-//
-//
-//}
+@RunWith(AndroidJUnit4::class)
+class GuessingTest {
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Test
+    fun guessingScreenIsDisplayed() {
+        initScreenWithDatabase()
+        composeRule.onNodeWithTag("guessingScreen").assertIsDisplayed()
+    }
+
+    @Test
+    fun guessingScreenContainsCorrectText() {
+        initScreenWithDatabase()
+        composeRule.onNodeWithTag("guessText").assertTextContains("Your turn to guess!")
+    }
+
+    @Test
+    fun guessesListIsDisplayed() {
+        initScreenWithDatabase()
+        composeRule.onNodeWithTag("guessesList").assertIsDisplayed()
+    }
+
+    @Test
+    fun guessingBarIsDisplayed() {
+        initScreenWithDatabase()
+        composeRule.onNodeWithTag("guessingBar").assertIsDisplayed()
+    }
+
+    @Test
+    fun guessingPreviewDisplaysGuessingScreen() {
+        initScreenWithoutDatabase()
+        composeRule.onNodeWithTag("guessingScreen").assertIsDisplayed()
+        composeRule.onNodeWithTag("guessText").assertTextContains("Your turn to guess!")
+        composeRule.onNodeWithTag("guessesList").assertIsDisplayed()
+        composeRule.onNodeWithTag("guessingBar").assertIsDisplayed()
+    }
+
+    @Test
+    fun guessIsDisplayedInGuessingList() {
+        initScreenWithDatabase()
+        composeRule.onNode(hasSetTextAction()).performTextInput("House")
+        composeRule.onNodeWithTag("guessButton").performClick()
+        composeRule.onNodeWithTag("guessItem").onChild().assertIsDisplayed()
+    }
+
+
+    private fun initGuessingDataBase(): DatabaseReference {
+        val db = Firebase.database
+        val guessGameId = "GameTestGuessesId"
+        db.useEmulator("10.0.2.2", 9000)
+        return Firebase.database.getReference("Guesses/$guessGameId")
+    }
+
+    private fun initScreenWithoutDatabase() {
+        composeRule.setContent {
+            BootcampComposeTheme {
+                GuessingPreview()
+            }
+        }
+    }
+
+    private fun initScreenWithDatabase() {
+        val database = initGuessingDataBase()
+
+        composeRule.setContent {
+            BootcampComposeTheme {
+                GuessingScreen(database)
+            }
+        }
+    }
+
+
+
+}
--- app/src/androidTest/java/com/github/freeman/bootcamp/auth/FirebaseAuthActivityTest.kt
+++ app/src/androidTest/java/com/github/freeman/bootcamp/auth/FirebaseAuthActivityTest.kt
@@ -3,16 +3,11 @@ package com.github.freeman.bootcamp.auth
 import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.*
 
 import com.github.freeman.bootcamp.MainMenuScreen
-import com.github.freeman.bootcamp.ui.theme.BootcampComposeTheme
-import junit.framework.Assert.assertNotNull
-import org.hamcrest.core.Is.`is`
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
