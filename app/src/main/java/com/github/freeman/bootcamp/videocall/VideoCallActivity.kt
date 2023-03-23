456028ecbe9476ec099358ee1d780e7a07814938
David Lacour <davidlacourgaletto@gmail.com>
2023-03-23 23:46:10 +0100

    video call, need help for testing because I can not test alone

---
+++ app/src/main/java/com/github/freeman/bootcamp/videocall/VideoCallActivity.kt
@@ -0,0 +1,59 @@
+package com.github.freeman.bootcamp.videocall
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.github.freeman.bootcamp.ui.theme.BootcampComposeTheme
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+// this is key to link the session to an agora account
+const val APP_ID = "46961364e2c74b4dbcc4d2ead7a09bee"
+
+@ExperimentalUnsignedTypes
+class VideoCallActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(
+                color = MaterialTheme.colors.background,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "room_screen"
+                ) {
+                    composable(route = "room_screen") {
+                        RoomScreen(onNavigate = navController::navigate)
+                    }
+                    composable(
+                        route = "video_screen/{roomName}",
+                        arguments = listOf(
+                            navArgument(name = "roomName") {
+                                type = NavType.StringType
+                            }
+                        )
+                    ) {
+                        val roomName = it.arguments?.getString("roomName") ?: return@composable
+                        VideoScreen(
+                            roomName = roomName,
+                            onNavigateUp = navController::navigateUp
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
