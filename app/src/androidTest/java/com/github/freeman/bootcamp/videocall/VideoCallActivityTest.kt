25e159cb83cacf54a87bf12f4323aabd0dddec02
David Lacour <davidlacourgaletto@gmail.com>
2023-03-25 13:13:23 +0100

    Fix adding favorites on small phones

--- app/src/androidTest/java/com/github/freeman/bootcamp/videocall/VideoCallActivityTest.kt
+++ app/src/androidTest/java/com/github/freeman/bootcamp/videocall/VideoCallActivityTest.kt
@@ -2,18 +2,14 @@ package com.github.freeman.bootcamp.videocall
 
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
-import com.github.freeman.bootcamp.recorder.AudioRecordingActivity
 import junit.framework.TestCase.assertEquals
-import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -51,14 +47,13 @@ class VideoCallActivityTest {
 
     @Test
     fun roomScreenTextFieldIsCorrectAndDisplayed() {
-        composeTestRule.onNode(hasTestTag("room_screen_text_field"))
-            .assertTextContains("Enter a room name")
+        roomScreenTextField().assertTextContains("Enter a room name")
             .assertIsDisplayed()
     }
 
     @Test
     fun roomScreenButtonTextIsCorrectAndDisplayedAndClickable() {
-        composeTestRule.onNode(hasTestTag("room_screen_button")).assertTextContains("Join")
+        roomScreenButton().assertTextContains("Join")
             .assertIsDisplayed()
             .assertHasClickAction()
     }
@@ -67,41 +62,32 @@ class VideoCallActivityTest {
     fun videoCallWithEmptyTextFieldDoesNotNavigatesAndDisplayCorrectText() {
         val route = navController.currentBackStackEntry?.destination?.route
         assertEquals(route, "room_screen")
-        composeTestRule.onNode(hasTestTag("room_screen_text_field")).performTextInput("");
+        roomScreenTextField().performTextInput("");
         ViewActions.closeSoftKeyboard()
-        composeTestRule.onNode(hasTestTag("room_screen_button")).performClick()
-        composeTestRule.onNode(hasTestTag("room_screen_button")).assertIsDisplayed()
-        composeTestRule.onNode(hasTestTag("room_screen_text_field")).assertIsDisplayed()
-        composeTestRule.onNode(hasTestTag("room_screen_error_field")).assertIsDisplayed().assertTextContains("The room can't be empty")
+        roomScreenButton().performClick()
+        roomScreenButton().assertIsDisplayed()
+        roomScreenTextField().assertIsDisplayed()
+        composeTestRule.onNode(hasTestTag("room_screen_error_field")).assertIsDisplayed()
+            .assertTextContains("The room can't be empty")
     }
-
-    // can not connect to agora on the continuous integration 
-    /*
-    @Test
-    fun videoCallNavHostNavigatesToVideoScreenFromRoomScreen() {
-        val route = navController.currentBackStackEntry?.destination?.route
-        assertEquals(route, "room_screen")
-        composeTestRule.onNode(hasTestTag("room_screen_text_field")).performTextInput("1");
-        ViewActions.closeSoftKeyboard()
-        composeTestRule.onNode(hasTestTag("room_screen_button")).performClick()
-        composeTestRule.onNode(hasTestTag("agora_video_view")).assertIsDisplayed()
-    }
-    */
 
     @Test
     fun leavingVideoScreenCloseVideoView() {
         val route = navController.currentBackStackEntry?.destination?.route
         assertEquals(route, "room_screen")
-        composeTestRule.onNode(hasTestTag("room_screen_text_field")).performTextInput("1");
+        roomScreenTextField().performTextInput("1");
         ViewActions.closeSoftKeyboard()
         roomScreenButton().performClick()
         pressBack()
-        composeTestRule.onNode(hasTestTag("room_screen_button")).assertIsDisplayed()
+        roomScreenButton().assertIsDisplayed()
         composeTestRule.onNode(hasTestTag("agora_video_view")).assertDoesNotExist()
     }
 
-    private fun  roomScreenButton(): SemanticsNodeInteraction{
-       return  composeTestRule.onNode(hasTestTag("room_screen_button"))
+    private fun roomScreenButton(): SemanticsNodeInteraction {
+        return composeTestRule.onNode(hasTestTag("room_screen_button"))
     }
 
+    private fun roomScreenTextField(): SemanticsNodeInteraction {
+        return composeTestRule.onNode(hasTestTag("room_screen_text_field"));
+    }
 }
