4a89ffb82d96dcd6c3dc2f37c246c9f9e919be57
ctaverni <clara.tavernier@epfl.ch>
2023-03-30 02:14:47 +0200

    Add even more tests

--- app/src/androidTest/java/com/github/freeman/bootcamp/auth/ProfileCreationTest.kt
+++ app/src/androidTest/java/com/github/freeman/bootcamp/auth/ProfileCreationTest.kt
@@ -7,9 +7,7 @@ import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.freeman.bootcamp.FirebaseEmulator
 import com.github.freeman.bootcamp.FirebaseSingletons
-import com.github.freeman.bootcamp.GameOptionsActivity
 import com.github.freeman.bootcamp.ui.theme.BootcampComposeTheme
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -43,9 +41,23 @@ class ProfileCreationTest {
     }
 
     @Test
+    fun usernameLabelIsDisplayed() {
+        initScreen()
+
+        composeRule.onNodeWithTag("usernameLabel", true).assertIsDisplayed()
+    }
+
+    @Test
     fun usernameBarContainsCorrectLabel() {
         initScreen()
+
         composeRule.onNodeWithTag("usernameLabel", true).assertTextContains("Username")
+    }
+
+    @Test
+    fun usernamePlaceholderIsDisplayed() {
+        initScreen()
+        composeRule.onNodeWithTag("usernamePlaceholder", true).assertIsDisplayed()
     }
 
     @Test
@@ -58,6 +70,12 @@ class ProfileCreationTest {
     fun doneButtonHasClickAction() {
         initScreen()
         composeRule.onNodeWithTag("usernameOkayButton").assertHasClickAction()
+    }
+
+    @Test
+    fun doneButtonHasCorrectDescription() {
+        initScreen()
+        composeRule.onNodeWithTag("usernameOkayButton").assertContentDescriptionContains( "okIconForUsername")
     }
 
     @Test
