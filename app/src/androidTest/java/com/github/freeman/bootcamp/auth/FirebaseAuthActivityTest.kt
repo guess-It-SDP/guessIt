dc9a28ebb32de50205d1de5a025492e0fc6c6a03
ctaverni <clara.tavernier@epfl.ch>
2023-04-20 21:27:14 +0200

    Add an authentification without Google, tested

--- app/src/androidTest/java/com/github/freeman/bootcamp/auth/FirebaseAuthActivityTest.kt
+++ app/src/androidTest/java/com/github/freeman/bootcamp/auth/FirebaseAuthActivityTest.kt
@@ -2,12 +2,15 @@ package com.github.freeman.bootcamp.auth
 
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiSelector
 import androidx.test.uiautomator.Until
 import com.github.freeman.bootcamp.MainMenuScreen
+import com.github.freeman.bootcamp.games.guessit.CreateJoinActivity
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -32,21 +35,54 @@ class FirebaseAuthActivityTest {
     }
 
     @Test
+    fun containsCorrectButtonsWhenNotSignedIn() {
+        composeRule.onNodeWithTag("google_sign_in_button").assertIsDisplayed()
+        composeRule.onNodeWithTag("anonymous_sign_in_button").assertIsDisplayed()
+        composeRule.onNodeWithTag("create_profile_button").assertIsNotDisplayed()
+        composeRule.onNodeWithTag("delete_button").assertIsNotDisplayed()
+        composeRule.onNodeWithTag("sign_out_button").assertIsNotDisplayed()
+    }
+
+    @Test
+    fun containsCorrectTextWhenNotSignedIn() {
+        composeRule.onNodeWithTag("sign_in_info").assertTextContains("Not signed in")
+    }
+
+    @Test
+    fun containsCorrectButtonsWhenAnonymouslySignedIn() {
+        composeRule.onNodeWithTag("anonymous_sign_in_button").performClick()
+        composeRule.onNodeWithTag("create_profile_button").assertIsDisplayed()
+        composeRule.onNodeWithTag("delete_button").assertIsDisplayed()
+    }
+
+    @Test
+    fun containsCorrectTextWhenAnonymouslySignedIn() {
+        composeRule.onNodeWithTag("anonymous_sign_in_button").performClick()
+        composeRule.onNodeWithTag("sign_in_info").assertTextContains("Signed in anonymously")
+    }
+
+    @Test
+    fun createProfileButtonsLaunchesTheActivity() {
+        Intents.init()
+
+        composeRule.onNodeWithTag("anonymous_sign_in_button").performClick()
+        composeRule.onNodeWithTag("create_profile_button").performClick()
+        Intents.intended(IntentMatchers.hasComponent(ProfileCreationActivity::class.java.name))
+
+        Intents.release()
+    }
+
+    @Test
     fun deleteGoogleAccountResultsInCorrectMessage() {
+        composeRule.onNodeWithTag("anonymous_sign_in_button").performClick()
         composeRule.onNodeWithTag("delete_button").performClick()
         composeRule.onNodeWithTag("sign_in_info").assertTextContains("Account deleted")
     }
 
     @Test
-    fun signOutResultsInCorrectMessage() {
-        composeRule.onNodeWithTag("sign_out_button").performClick()
-        composeRule.onNodeWithTag("sign_in_info").assert(hasText("Signed out") or hasText("Not signed in"))
-    }
-
-    @Test
     fun signInResultsInCorrectLayout() {
 
-        composeRule.onNodeWithTag("sign_in_button").performClick()
+        composeRule.onNodeWithTag("google_sign_in_button").performClick()
         device.wait(
             Until.findObject(By.textContains("Google")), 30000
         )
