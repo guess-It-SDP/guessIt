6d77be09f90c78dea3223ccbda2955854e538ee4
David <davidlacourgaletto@gmail.com>
2023-05-03 14:59:26 +0200

    Video screen back to default

--- app/src/main/java/com/github/freeman/bootcamp/videocall/VideoScreen.kt
+++ app/src/main/java/com/github/freeman/bootcamp/videocall/VideoScreen.kt
@@ -5,12 +5,10 @@ import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -19,7 +17,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import io.agora.agorauikit_android.AgoraConnectionData
-import io.agora.agorauikit_android.AgoraSettings
 import io.agora.agorauikit_android.AgoraVideoViewer
 
 /**
@@ -61,29 +58,18 @@ fun VideoScreen(
         onNavigateUp()
     }
     if(viewModel.hasAudioPermission.value && viewModel.hasCameraPermission.value && !testing) {
-        val agoraSettings = AgoraSettings()
-        agoraSettings.enabledButtons = mutableSetOf(
-            //  AgoraSettings.BuiltinButton.CAMERA,
-            //  AgoraSettings.BuiltinButton.MIC,
-            // AgoraSettings.BuiltinButton.FLIP
-        )
-
-
         AndroidView(
             factory = {
-
                 AgoraVideoViewer(
                     it, connectionData = AgoraConnectionData(
-                        appId = APP_ID,
-
-                    ), style = AgoraVideoViewer.Style.FLOATING,
-                    agoraSettings = agoraSettings
+                        appId = APP_ID
+                    )
                 ).also {
                     it.join(roomName)
                     agoraView = it
                 }
             },
-            modifier = Modifier.height(100.dp).testTag("agora_video_view")
+            modifier = Modifier.fillMaxSize().testTag("agora_video_view")
         )
     }
 }
