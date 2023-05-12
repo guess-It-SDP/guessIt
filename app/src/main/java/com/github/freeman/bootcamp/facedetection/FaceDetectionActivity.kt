da05ab9b9607b222be6cc8a2dd7eae63c8f278f3
David <davidlacourgaletto@gmail.com>
2023-05-12 14:02:49 +0200

    facedetection code refinement
    code has been modularize, simplified and commented so faceDertection is
    easier to use

--- app/src/main/java/com/github/freeman/bootcamp/facedetection/FaceDetectionActivity.kt
+++ app/src/main/java/com/github/freeman/bootcamp/facedetection/FaceDetectionActivity.kt
@@ -1,182 +1,78 @@
 package com.github.freeman.bootcamp.facedetection
 
+import android.content.Context
+import android.content.res.Resources
 import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.unit.dp
-
-/*
- * Copyright 2020 Google LLC. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import android.graphics.*
-import androidx.core.graphics.toRectF
-
 import com.google.mlkit.vision.face.FaceDetection
-
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.github.freeman.bootcamp.*
-import com.github.freeman.bootcamp.ui.theme.BootcampComposeTheme
-import com.github.freeman.bootcamp.utilities.firebase.FirebaseUtilities
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
 import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.face.*
-
-import java.io.InputStream
-
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Context
 import android.graphics.BitmapFactory
-
-import android.net.Uri
-
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material.icons.rounded.*
+import android.graphics.drawable.Drawable
 import androidx.compose.runtime.*
-
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import coil.compose.rememberAsyncImagePainter
-import com.github.freeman.bootcamp.EditProfileActivity.Companion.DONE
-import com.github.freeman.bootcamp.EditProfileActivity.Companion.EMPTY_ERROR
-import com.github.freeman.bootcamp.EditProfileActivity.Companion.ENTER_VALUE
-import com.github.freeman.bootcamp.EditProfileActivity.Companion.SET_VALUE
-import com.github.freeman.bootcamp.EditProfileActivity.Companion.TOPBAR_TEXT
-import com.github.freeman.bootcamp.EditProfileActivity.Companion.USER_NAME
 import com.github.freeman.bootcamp.R
-import com.github.freeman.bootcamp.ui.theme.BootcampComposeTheme
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.ktx.database
-
-import com.google.firebase.storage.ktx.storage
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.io.IOException
-
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.foundation.Image as Image1
+import com.google.mlkit.vision.face.Face
 
+/**
+ * A class that provides face detection functionality and draws on it
+ */
 class FaceDetectionActivity : ComponentActivity() {
+    companion object {
+        const val HAT_FOREHEAD_FACTOR = -1.5 // The factors by which the distance of the hat from the boundingBox should be adjusted.
+        const val HAT_LEFT_HEAD_FACTOR = -3.0
+        const val HAT_RIGHT_HEAD_FACTOR = 3.0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var image : InputImage? = null
-
-            drawSquaresOnBitmapImage()
+            var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+            var context = LocalContext.current
+            var faceBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.hat)
+            bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.my_image2)
+                .copy(Bitmap.Config.ARGB_8888, true)
+            drawHatOnBitmapImage(bitmap, faceBitmap)
         }
     }
 
+    /**
+     * Draws a hat on top of faces detected in a given bitmap image, using the provided overlay picture.
+     *
+     * @param bitmap The bitmap image on which to draw the hat.
+     * @param overlayPic The overlay picture to use for the hat.
+     */
     @Composable
-    fun drawSquaresOnBitmapImage() {
-        var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+    fun drawHatOnBitmapImage(bitmap: Bitmap?, overlayPic : Bitmap ) {
         var faceDetected by remember { mutableStateOf(false) }
-        val context = LocalContext.current
         LaunchedEffect(true) {
-            bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.my_image2)
-                .copy(Bitmap.Config.ARGB_8888, true)
             val image = InputImage.fromBitmap(bitmap!!, 0)
-            val options = FaceDetectorOptions.Builder()
-                .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
-                .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
-                .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
-                .build()
+            val options = options()
             val detector = FaceDetection.getClient(options)
             detector.process(image)
                 .addOnSuccessListener { faces ->
-                    // Process the faces here
                     val canvas = Canvas(bitmap!!)
-                    val paint = Paint()
-                    paint.color = Color.Red.toArgb()
-                    paint.style = Paint.Style.STROKE
-                    paint.strokeWidth = 16.0f
-
-                    /*
+                    val paint = paint()
                     faces.forEach { face ->
-                        val boundingBox = face.boundingBox
-                        val rect = Rect(boundingBox.left, boundingBox.top, boundingBox.right, boundingBox.bottom)
-                        canvas.drawRect(rect, paint)
+                        drawHat(face, canvas, overlayPic, paint)
                     }
-                    */
-
-
-                    val faceBitmap = BitmapFactory.decodeResource(context.resources, R.drawable.hat)
-                    faces.forEach { face ->
-                        val boundingBox = face.boundingBox
-                        val rect = Rect(boundingBox.left-boundingBox.width()/3, Math.floor(face.boundingBox.top-face.boundingBox.height()/1.5).toInt(), boundingBox.right+boundingBox.width()/3, Math.floor(face.boundingBox.bottom-face.boundingBox.height()/1.5).toInt())
-                        canvas.drawBitmap(faceBitmap, null, rect, paint)
-                    }
-
-
 
                     faceDetected = true
 
@@ -196,120 +92,60 @@ class FaceDetectionActivity : ComponentActivity() {
             }
         }
     }
+}
+private fun paint(): Paint {
+    val paint = Paint()
+    paint.color = Color.Red.toArgb()
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = 16.0f
+    return paint
+}
+private fun options(): FaceDetectorOptions {
+    val options = FaceDetectorOptions.Builder()
+        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
+        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+        .build()
+    return options
+}
 
+/**
+ * Draws a hat on the given face in the canvas using the provided face bitmap and paint object.
+ *
+ * @param face       The face object representing the detected face.
+ * @param canvas     The canvas object where the hat will be drawn.
+ * @param faceBitmap The bitmap object representing the face image.
+ * @param paint      The paint object used to draw the hat.
+ */
+private fun drawHat(face: Face, canvas: Canvas, overlayPic: Bitmap, paint: Paint) {
+    val boundingBox = face.boundingBox
+    val rect = Rect(
+        boundingBox.left + adjustBound(
+            boundingBox.width(),
+            FaceDetectionActivity.HAT_LEFT_HEAD_FACTOR
+        ), face.boundingBox.top + adjustBound(
+            face.boundingBox.height(),
+            FaceDetectionActivity.HAT_FOREHEAD_FACTOR
+        ), boundingBox.right + adjustBound(
+            boundingBox.width(),
+            FaceDetectionActivity.HAT_RIGHT_HEAD_FACTOR
+        ), face.boundingBox.bottom + adjustBound(
+            face.boundingBox.height(),
+            FaceDetectionActivity.HAT_FOREHEAD_FACTOR
+        )
+    )
+    canvas.drawBitmap(overlayPic, null, rect, paint)
+}
 
-
-    private fun detectFaces(image: InputImage) {
-        // [START set_detector_options]
-        val options = FaceDetectorOptions.Builder()
-            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
-            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
-            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
-            .setMinFaceSize(0.15f)
-            .enableTracking()
-            .build()
-        // [END set_detector_options]
-
-        // [START get_detector]
-        val detector = FaceDetection.getClient(options)
-        // Or, to use the default option:
-        // val detector = FaceDetection.getClient();
-        // [END get_detector]
-
-        // [START run_detector]
-        val result = detector.process(image)
-            .addOnSuccessListener { faces ->
-                // Task completed successfully
-                // [START_EXCLUDE]
-                // [START get_face_info]
-                for (face in faces) {
-                    val bounds = face.boundingBox
-                    val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
-                    val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
-
-                    // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
-                    // nose available):
-                    val leftEar = face.getLandmark(FaceLandmark.LEFT_EAR)
-                    leftEar?.let {
-                        val leftEarPos = leftEar.position
-                    }
-
-                    // If classification was enabled:
-                    if (face.smilingProbability != null) {
-                        val smileProb = face.smilingProbability
-                    }
-                    if (face.rightEyeOpenProbability != null) {
-                        val rightEyeOpenProb = face.rightEyeOpenProbability
-                    }
-
-                    // If face tracking was enabled:
-                    if (face.trackingId != null) {
-                        val id = face.trackingId
-                    }
-                }
-                // [END get_face_info]
-                // [END_EXCLUDE]
-
-            }
-            .addOnFailureListener { e ->
-                // Task failed with an exception
-                // ...
-            }
-        // [END run_detector]
-    }
-
-    private fun faceOptionsExamples() {
-        // [START mlkit_face_options_examples]
-        // High-accuracy landmark detection and face classification
-        val highAccuracyOpts = FaceDetectorOptions.Builder()
-            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
-            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
-            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
-            .build()
-
-        // Real-time contour detection
-        val realTimeOpts = FaceDetectorOptions.Builder()
-            .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
-            .build()
-        // [END mlkit_face_options_examples]
-    }
-
-    private fun processFaceList(faces: List<Face>) {
-        // [START mlkit_face_list]
-        for (face in faces) {
-            val bounds = face.boundingBox
-            val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
-            val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
-
-            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
-            // nose available):
-            val leftEar = face.getLandmark(FaceLandmark.LEFT_EAR)
-            leftEar?.let {
-                val leftEarPos = leftEar.position
-            }
-
-            // If contour detection was enabled:
-            val leftEyeContour = face.getContour(FaceContour.LEFT_EYE)?.points
-            val upperLipBottomContour = face.getContour(FaceContour.UPPER_LIP_BOTTOM)?.points
-
-            // If classification was enabled:
-            if (face.smilingProbability != null) {
-                val smileProb = face.smilingProbability
-            }
-            if (face.rightEyeOpenProbability != null) {
-                val rightEyeOpenProb = face.rightEyeOpenProbability
-            }
-
-            // If face tracking was enabled:
-            if (face.trackingId != null) {
-                val id = face.trackingId
-            }
-        }
-        // [END mlkit_face_list]
-    }
-
-
-
+/**
+ * Adjusts the distance of the face bounding box based on a given factor.
+ *
+ * @param boundsDistance The distance of the face bounding box to be adjusted.
+ * @param factor         The factor by which the distance of the bounding box should be adjusted.
+ * @return The adjusted distance of the face bounding box.
+ */
+private fun adjustBound(boundsDistance: Int, factor: Double): Int {
+    return Math.floor(boundsDistance / factor).toInt();
 }
 
 @Composable
@@ -321,4 +157,5 @@ fun FaceDetectionScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-    }}
+    }
+}
