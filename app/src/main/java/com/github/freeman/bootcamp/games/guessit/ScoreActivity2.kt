a3c5a4e46880cbe059780eadc2f0df7ed26297a0
David <davidlacourgaletto@gmail.com>
2023-05-25 23:03:04 +0200

    I changed scoreActivity2
    I modularize the code a bit(but still needs more), still need to add some tests and manualy
    checks And the code could need some constants as well

--- app/src/main/java/com/github/freeman/bootcamp/games/guessit/ScoreActivity2.kt
+++ app/src/main/java/com/github/freeman/bootcamp/games/guessit/ScoreActivity2.kt
@@ -5,21 +5,28 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import com.github.freeman.bootcamp.R
 import com.github.freeman.bootcamp.games.guessit.ScoreActivity.Companion.SCORES_TITLE
 import com.github.freeman.bootcamp.games.guessit.ScoreActivity.Companion.size
@@ -37,238 +44,106 @@ import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class ScoreActivity2 : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val gameID = intent.getStringExtra(getString(R.string.gameId_extra))
         val dbRef = getGameDBRef(this, gameID.toString())
         setContent {
             BootcampComposeTheme {
-                ScoreScreen2(dbRef)
+                val context = LocalContext.current
+
+                // Get the Ids of all players in this game
+                val playerIds = remember { mutableStateOf(mapOf<String, Map<String, Int>>()) }
+                FirebaseUtilities.databaseGetMap(dbRef.child(context.getString(R.string.players_path)))
+                    .thenAccept {
+                        playerIds.value = it as HashMap<String, Map<String, Int>>
+                    }
+
+                // Get the player ID to score map
+                val playersToScores = obtainPlayersToScores(dbRef, playerIds, context)
+
+                val scores = turnIntoPairs(playersToScores)
+                val usernames = fetchUserNames(scores)
+                val usersToScores = usernamesToScores(scores, usernames).sortedWith(compareByDescending { it.second })
+
+                    Column() {
+                        CurrentScoreboard(usersToScores = usersToScores)
+                        VideoScreen(roomName = gameID ?: "1", testing = false)
+                    }
             }
         }
     }
-
     companion object {
         const val size = 200
-        const val SCORES_TITLE = "Scores"
-        const val FINAL_SCORES_TITLE = "Final Scores"
-    }
-}
-
-/***
- * This function is necessary as the scoreboard takes Pairs as input, not Maps
- */
-@Composable
-fun turnIntoPairs2(playersToScores: Map<String, MutableState<Int>>): List<Pair<String, Int>> {
-    // Converts the map of players to mutable state of int into a list of pairs ID-score instead
-    val scorePairs = ArrayList<Pair<String, Int>>()
-    if (playersToScores.isNotEmpty()) {
-        for (entry in playersToScores) {
-            scorePairs.add(Pair(entry.key, entry.value.value))
-        }
-    }
-
-    return scorePairs
-}
-
-@Composable
-fun fetchUserNames2(playerIds: List<Pair<String, Int>>): Map<String, MutableState<String>> {
-    val dbRef = Firebase.database.reference
-    val usernames = HashMap<String, MutableState<String>>()
-
-    // Initialise the values of the map player ID to username
-    for (entry in playerIds) {
-        val id = entry.first
-        usernames[id] = remember { mutableStateOf("") }
-    }
-
-    // Get the usernames of all players in this game
-    for (entry in playerIds) {
-        val id = entry.first
-        val drefUsername = dbRef
-            .child(LocalContext.current.getString(R.string.profiles_path))
-            .child(id)
-            .child(LocalContext.current.getString(R.string.username_path))
-        FirebaseUtilities.databaseGet(drefUsername)
-            .thenAccept {
-                usernames[id]?.value = it
-            }
-    }
-
-    return usernames
-}
-
-@Composable
-fun usernamesToScores2(
-    scores: List<Pair<String, Int>>,
-    usernames: Map<String, MutableState<String>>
-): List<Pair<String?, Int>> {
-
-    // Map the usernames to their corresponding scores
-    val usersToScores = ArrayList<Pair<String?, Int>>()
-    for (entry in scores) {
-        val id = entry.first
-        val score = entry.second
-        val username = usernames[id]?.value
-        usersToScores.add(Pair(username, score))
-    }
-
-    return usersToScores
-}
-
-fun updateScoreMap2(playersToScores: Map<String, MutableState<Int>>, id: String, snapshot: DataSnapshot) {
-    if (snapshot.exists() && snapshot.key == "score") {
-        val score = snapshot.getValue<Int>()
-        if (score != null) {
-            playersToScores[id]?.value = score
-        }
     }
 }
 
 @Composable
-fun obtainPlayersToScores2(
-    dbRef: DatabaseReference,
-    playerIds: MutableState<Map<String, Map<String, Int>>>,
-    context: Context
-): HashMap<String, MutableState<Int>> {
-    // Initialise the values of the map player ID to score
-    val playersToScores = HashMap<String, MutableState<Int>>()
-    for (id in playerIds.value.keys) {
-        playersToScores[id] = remember { mutableStateOf(-1) }
-    }
-
-    // Observe the points of all players to update the scoreboard
-    for (id in playerIds.value.keys) {
-        dbRef
-            .child(context.getString(R.string.players_path))
-            .child(id)
-            .addChildEventListener(object : ChildEventListener {
-                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                    updateScoreMap(playersToScores, id, snapshot)
-                }
-
-                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                    updateScoreMap(playersToScores, id, snapshot)
-                }
-
-                override fun onChildRemoved(snapshot: DataSnapshot) {
-                    updateScoreMap(playersToScores, id, snapshot)
-                }
-
-                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-                    updateScoreMap(playersToScores, id, snapshot)
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    // No particular action needs to be taken in this case
-                }
-            })
-    }
-    return playersToScores
-}
-
-@Composable
-fun ScoreScreen2(
-    dbRef: DatabaseReference,
-    testingPlayersToScores: HashMap<String, MutableState<Int>> = HashMap(),
-    testingUsersToScores:  List<Pair<String?, Int>> = listOf()
-) {
+fun CurrentScoreboard(usersToScores: List<Pair<String?, Int>>) {
     val context = LocalContext.current
-
-    // Get the Ids of all players in this game (IDs = playerIds.value.keys)
-    val playerIds = remember { mutableStateOf(mapOf<String, Map<String, Int>>()) }
-    FirebaseUtilities.databaseGetMap(dbRef.child(context.getString(R.string.players_path)))
-        .thenAccept {
-            playerIds.value = it as HashMap<String, Map<String, Int>>
-        }
-
-    // Get the player ID to score map
-    var playersToScores = obtainPlayersToScores(dbRef, playerIds, context)
-
-    // Dependency injection for testing purposes
-    if (testingPlayersToScores.size > 0) {
-        playersToScores = testingPlayersToScores
-    }
-
-    val scores = turnIntoPairs(playersToScores)
-    val usernames = fetchUserNames(scores)
-    var usersToScores = usernamesToScores(scores, usernames).sortedWith(compareByDescending { it.second })
-
-    // Dependency injection for testing purposes
-    if (testingPlayersToScores.size > 0) {
-        usersToScores = testingUsersToScores
-    }
-
-   Column() {
-       Row(
-           modifier = Modifier
-               .fillMaxWidth()
-               .height(size.dp),
-           horizontalArrangement = Arrangement.End,
-           verticalAlignment = Alignment.Top
-       ) {
-           Spacer(modifier = Modifier.weight(1f))
-
-           val nbPlayers = usersToScores.size
-           Scoreboard(
-               playerScores = usersToScores,
-               modifier = Modifier
-                   .width((0.475 * size).dp)
-                   .height(((0.225 + nbPlayers * 0.11) * size).dp)
-                   .testTag("scoreboard")
-           )
-       }
-       VideoScreen(roomName = "1", testing = false)
-   }
-}
-
-@Composable
-fun Scoreboard2(playerScores: List<Pair<String?, Int>>, modifier: Modifier) {
-    var size by remember { mutableStateOf(IntSize.Zero) }
-
     Box(
-        contentAlignment = Alignment.TopEnd,
-        modifier = modifier
-            .padding(2.dp)
-            .fillMaxHeight()
-            .background(Color.LightGray, RoundedCornerShape(10.dp))
-            .onSizeChanged { size = it }
+        modifier = Modifier
+            .background(Color.Blue, RoundedCornerShape(16.dp))
+            .padding(8.dp)
+            .border(
+                BorderStroke(
+                    width = 4.dp,
+                    brush = Brush.linearGradient(
+                        colors = FinalActivity.BLUES
+                    )
+                ),
+                shape = RoundedCornerShape(16.dp),
+            )
+            .shadow(
+                elevation = 2.dp,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .testTag("endScoreboard")
     ) {
-        Column(
-            modifier = Modifier
-                .padding(6.dp)
-                .fillMaxSize()
+        Column (
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = SCORES_TITLE,
-                style = MaterialTheme.typography.subtitle1,
+                text = ScoreActivity.SCORES_TITLE,
+                color = Color.White,
+                style = MaterialTheme.typography.h4,
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
-                    .testTag("scoresTitle")
+                    .padding(horizontal = 20.dp, vertical = 10.dp)
+                    .testTag("endScoresTitle")
             )
+
             Spacer(modifier = Modifier.height(2.dp))
-            playerScores.forEach { (name, score) ->
+            Divider(color = FinalActivity.BLUES[1], thickness = 4.dp)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            usersToScores.forEach { (name, score) ->
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     if (name != null) {
                         Text(
                             text = name,
-                            style = MaterialTheme.typography.body2,
+                            color = Color.White,
+                            style = MaterialTheme.typography.body1,
                             modifier = Modifier
                                 .weight(1f)
-                                .testTag(name)
+                                .padding(horizontal = 20.dp, vertical = 10.dp)
+                                .testTag("end$name")
                         )
-
                         Text(
                             text = score.toString(),
-                            style = MaterialTheme.typography.body2,
-                            modifier = Modifier.testTag("score")
+                            color = Color.White,
+                            style = MaterialTheme.typography.body1,
+                            modifier = Modifier
+                                .padding(horizontal = 20.dp, vertical = 10.dp)
+                                .testTag("endScore")
                         )
                     }
                 }
-                Divider(color = Color.Black, thickness = 1.dp)
+                Divider(color = Color.White, thickness = 1.dp)
             }
         }
     }
