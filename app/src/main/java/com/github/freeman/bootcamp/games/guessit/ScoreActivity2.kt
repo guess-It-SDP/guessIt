2fd01d9346ae711a649549a04558d39de9562e9f
ctaverni <clara.tavernier@epfl.ch>
2023-05-26 10:46:17 +0200

    UI in ScoreActivity2

--- app/src/main/java/com/github/freeman/bootcamp/games/guessit/ScoreActivity2.kt
+++ app/src/main/java/com/github/freeman/bootcamp/games/guessit/ScoreActivity2.kt
@@ -1,48 +1,31 @@
 package com.github.freeman.bootcamp.games.guessit
 
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import com.github.freeman.bootcamp.R
-import com.github.freeman.bootcamp.games.guessit.ScoreActivity.Companion.SCORES_TITLE
-import com.github.freeman.bootcamp.games.guessit.ScoreActivity.Companion.size
 import com.github.freeman.bootcamp.games.guessit.ScoreActivity2.Companion.SCORES_RECAP_BOARD_TAG
 import com.github.freeman.bootcamp.ui.theme.BootcampComposeTheme
 import com.github.freeman.bootcamp.utilities.firebase.FirebaseUtilities
 import com.github.freeman.bootcamp.utilities.firebase.FirebaseUtilities.getGameDBRef
 import com.github.freeman.bootcamp.videocall.VideoScreen
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
-import com.google.firebase.ktx.Firebase
 
 class ScoreActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,9 +52,11 @@ class ScoreActivity2 : ComponentActivity() {
                     scores, usernames
                 ).sortedWith(compareByDescending { it.second })
 
-                Column() {
-                    CurrentScoreboard(usersToScores = usersToScores)
-                    VideoScreen(roomName = gameID ?: "1", testing = false)
+                Surface {
+                    Column {
+                        CurrentScoreboard(usersToScores = usersToScores)
+                        VideoScreen(roomName = gameID ?: "1", testing = false)
+                    }
                 }
             }
         }
@@ -79,22 +64,23 @@ class ScoreActivity2 : ComponentActivity() {
 
     companion object {
         const val size = 200
-        const  val SCORES_RECAP_TITLE = "Scores, next round coming soon"
         const  val SCORES_RECAP_BOARD_TAG = "Scores, next round coming soon"
     }
 }
 
 @Composable
 fun CurrentScoreboard(usersToScores: List<Pair<String?, Int>>) {
-    val context = LocalContext.current
     Box(
         modifier = Modifier
-            .background(Color.Blue, RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.primaryContainer, RoundedCornerShape(16.dp))
             .padding(8.dp)
             .border(
                 BorderStroke(
                     width = 4.dp, brush = Brush.linearGradient(
-                        colors = FinalActivity.BLUES
+                        colors = listOf(
+                            MaterialTheme.colorScheme.primary,
+                            MaterialTheme.colorScheme.onPrimaryContainer
+                        )
                     )
                 ),
                 shape = RoundedCornerShape(16.dp),
@@ -110,8 +96,7 @@ fun CurrentScoreboard(usersToScores: List<Pair<String?, Int>>) {
         ) {
             Text(
                 text = ScoreActivity.SCORES_TITLE,
-                color = Color.White,
-                style = MaterialTheme.typography.h4,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
                     .padding(horizontal = 20.dp, vertical = 10.dp)
@@ -119,7 +104,7 @@ fun CurrentScoreboard(usersToScores: List<Pair<String?, Int>>) {
             )
 
             Spacer(modifier = Modifier.height(2.dp))
-            Divider(color = FinalActivity.BLUES[1], thickness = 4.dp)
+            Divider(color = MaterialTheme.colorScheme.primary, thickness = 4.dp)
 
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -130,8 +115,7 @@ fun CurrentScoreboard(usersToScores: List<Pair<String?, Int>>) {
                     if (name != null) {
                         Text(
                             text = name,
-                            color = Color.White,
-                            style = MaterialTheme.typography.body1,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer,
                             modifier = Modifier
                                 .weight(1f)
                                 .padding(horizontal = 20.dp, vertical = 10.dp)
@@ -139,15 +123,14 @@ fun CurrentScoreboard(usersToScores: List<Pair<String?, Int>>) {
                         )
                         Text(
                             text = score.toString(),
-                            color = Color.White,
-                            style = MaterialTheme.typography.body1,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer,
                             modifier = Modifier
                                 .padding(horizontal = 20.dp, vertical = 10.dp)
                                 .testTag("endScore")
                         )
                     }
                 }
-                Divider(color = Color.White, thickness = 1.dp)
+                Divider(color = MaterialTheme.colorScheme.primary, thickness = 1.dp)
             }
         }
     }
